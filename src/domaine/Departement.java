1f8c2e4796b191f4016c4dc47c5aedb5bb04d6f1
Damien Faraone <damien.faraone@hes.adhes.hesge.ch>
2017-03-10 12:59:26 +0100

    Ca marche paaaaas

---
+++ src/domaine/Departement.java
@@ -0,0 +1,34 @@
+package domaine;
+
+/** 
+ * Entité représentant un Departement
+ *
+ * @author Peter DAEHNE - HEG Genève
+ * @version 0.1
+ */
+public class Departement {
+    private int noDept;
+    private String nomDept;
+    private Lieu lieu;
+    /* Constructeur */
+    public Departement (int noDept, String nomDept, Lieu lieu) {
+        this.noDept = noDept;
+        this.nomDept = nomDept;
+        this.lieu = lieu;
+    } // Constructeur
+
+    //GETTERS
+    public int getNo(){
+        return this.noDept;
+    }    
+    public String getNom(){
+        return nomDept;
+    }    
+    public Lieu getLieu(){
+        return lieu;
+    }
+
+    public String toString(){
+        return this.noDept + " - " + this.nomDept + " - " + this.lieu;
+    }
+} // Departement
---
+++ src/domaine/Lieu.java
@@ -0,0 +1,29 @@
+package domaine;
+
+/** 
+ * Entité représentant un Lieu
+ *
+ * @author Peter DAEHNE - HEG Genève
+ * @version 0.1
+ */
+public class Lieu {
+    private int noLieu;
+    private String nomLieu;
+    /** Constructeur */
+    public Lieu (int noLieu, String nomLieu) {
+        this.noLieu = noLieu;
+        this.nomLieu = nomLieu;
+    } // Constructeur
+    
+    //GETTERS
+    public int getNo(){
+        return noLieu;
+    }
+    public String getNom(){
+        return nomLieu;
+    }
+    
+    public String toString(){
+        return "[ " + noLieu + " - " + nomLieu + " ]";
+    }
+} // Lieu
